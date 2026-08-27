e14abe319e7331d9a5c7672cdf9e103049865935
mobility-pro <98306595+mobility-pro@users.noreply.github.com>
2026-08-27 08:24:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+import re
+import ast
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in mobility_advanced_item_dialog/__init__.py
-from mobility_advanced_item_dialog import __version__ as version
+with open("mobility_advanced_item_dialog/__init__.py") as f:
+	for line in f:
+		match = re.match(r"^__version__\s*=\s*(.+)$", line)
+		if match:
+			version = ast.literal_eval(match.group(1))
+			break
 
 setup(
 	name="mobility_advanced_item_dialog",
